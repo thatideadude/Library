c6c6c90e10612f00352b40d378d7236fd5252965
thatIdeaDude <thatideadude@gmail.com>
2024-10-28 19:28:18 +0000

    Starting library app

---
+++ library-project/library.js
@@ -0,0 +1,12 @@
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.status}`;
+  }
+}
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
+
+console.log(theHobbit.info());
